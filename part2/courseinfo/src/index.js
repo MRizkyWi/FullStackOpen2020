980e89014ef06c253b1487d64b18a14aa803e408
Rizky Widyayulianto <rizky.widyayulianto@gmail.com>
2020-06-27 13:45:52 +0700

    finished part 2.4 & 2.5

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,42 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Course from './components/Course'
-
-// const Total = (props) => {
-//   return (
-//     <div>
-//       <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises} </p>
-//     </div>
-//   )
-// }
-
-const App = () => {  
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        id: 1,
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        id: 2,
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        id: 3,
-        name: 'State of a component',
-        exercises: 14
-      },
-    ]
-  }
-  return (
-    <div>
-      <Course course={course}/>
-    </div>
-  )
-}
+import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
