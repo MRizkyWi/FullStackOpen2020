b9e39c3f55d603f1bfc0c2a2117553f6bacebd6a
Rizky Widyayulianto <rizky.widyayulianto@gmail.com>
2020-08-28 18:26:49 +0700

    add exercise 3.2

--- part3/persons/index.js
+++ part3/persons/index.js
@@ -40,14 +40,24 @@ app.get('/info', (request, response) => {
     var now = new Date;
    
     response.send('<p>Phonebook has info for '+ countPersons() + ' people </p><p>'+ now.toString() + '</p>')
-  })
+})
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get('/info', (request, response) => {
-    response.json(persons)
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    // console.log("id: ", id)
+    
+    const person = persons.find(person => person.id === id)
+    // console.log(person.name)
+
+    if (person){
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
