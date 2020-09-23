b831d4c765c89a0c342dcf8b357c5738011256cd
Rizky Widyayulianto <rizky.widyayulianto@gmail.com>
2020-09-23 09:44:46 +0700

    add exercise 3.9

---
+++ part2/phonebook/build/precache-manifest.2c6a6a1bfa2732d8761769c4d4380dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab87f1683326a42d451cb2d7d17da0d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a4e6da45fb9dab79094",
+    "url": "/static/css/main.17090786.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4e6da45fb9dab79094",
+    "url": "/static/js/main.e89574a0.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
