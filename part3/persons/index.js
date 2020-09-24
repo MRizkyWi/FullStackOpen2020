e5d1fe9bfb5437377dbb6626afea64c976d784d9
Rizky Widyayulianto <rizky.widyayulianto@gmail.com>
2020-09-24 21:59:46 +0700

    add exercise 3.16 & 3.17*

--- part3/persons/index.js
+++ part3/persons/index.js
@@ -45,7 +45,7 @@ app.get('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204)
@@ -79,6 +79,21 @@ app.post('/api/persons', (request, response) => {
         console.log('person saved')
         response.json(person)
     })    
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
