ba335dc8ac078ea42217b6cb7c1b054b13ddb2e7
Rizky Widyayulianto <rizky.widyayulianto@gmail.com>
2020-09-24 10:39:35 +0700

    add exercise 3.12

---
+++ part3/persons/mongo.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose')
+
+// check whether command include password or not
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+// get password
+const password = process.argv[2]
+
+// parse url and connect
+const url =
+  `mongodb+srv://admin:${password}@cluster0.jjypb.mongodb.net/phonebook?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// determine database schema of Person
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: Number,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length >= 4){
+    // get the name and number
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    // find largest id
+    let id = 1
+    Person.find({}).then(result => {
+        if (result.length != 0){
+            const varPerson = result[result.length - 1]
+            id = varPerson.id
+        }
+    })
+
+    // make new person to add to database
+    const person = new Person({
+        name: name,
+        number: number,
+        id: id + 1,
+    })
+        
+    person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    })
+} else {
+    // print all person in phonebook
+    console.log("phonebook")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+
+
+
+
